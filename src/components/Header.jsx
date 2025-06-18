845c213182e56d81bd5d0b5350cb043993ce3c91
sokilskills <vprovorotov@amil.com>
2025-06-18 15:30:24 +0300

    feat: added Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header className="mb-8">
+      <h1 className="text-3xl font-bold text-center mb-2 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent">
+        Встигни все, з’їж свою жабу
+      </h1>
+      <p className="text-center text-gray-500 dark:text-gray-400">
+        Організуйте свої завдання ефективно
+      </p>
+    </header>
+  );
+};
+
+export default Header;
