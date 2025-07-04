e52119d5cc4a86ee3b50d157a457dab0fd38be65
sokilskills <vprovorotov@amil.com>
2025-07-04 11:22:28 +0300

    feat: added filters

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import projectsReduser from "./projects/projectsSlice";
 import todosReducer from "./todos/todosSlice";
+import filterReduce from "./filters/filtersSlice";
 
 const store = configureStore({
   reducer: {
     projects: projectsReduser,
     todos: todosReducer,
+    filters: filterReduce,
   },
 });
 
