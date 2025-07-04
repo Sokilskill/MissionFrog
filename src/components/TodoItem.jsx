4e8efd10f811ed0991972db3518f80b133d7bb92
sokilskills <vprovorotov@amil.com>
2025-07-04 11:54:01 +0300

    feat: add EditModal and deleteModal, formattedDate
    1. Відкриття модалки редагувати
    2. ВІдкриття модалки підтвердження видалення завдання
    3. Форматуванян та відображення дати створення та виконанян завдання

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -5,44 +5,48 @@ import {
   deleteTodo,
   updateTodo,
 } from "../redux/todos/todosSlice";
-
-function getPriorityClass(priority) {
-  const classes = {
-    high: "bg-red-100 dark:bg-red-900/30 text-red-800 dark:text-red-200",
-    medium:
-      "bg-yellow-100 dark:bg-yellow-900/30 text-yellow-800 dark:text-yellow-200",
-    low: "bg-green-100 dark:bg-green-900/30 text-green-800 dark:text-green-200",
-  };
-  return classes[priority] || "";
-}
+import { formatTodoDate, getPriorityClass } from "../utils";
+import { useState } from "react";
+import TodoEditModal from "./TodoEditModal";
+import Modal from "./Modal";
 
 const TodoItem = ({ todo, project }) => {
   const dispatch = useDispatch();
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 
-  const handleEdit = () => {
-    const newTitle = prompt("Введіть нову назву завдання:", todo.title);
-    if (newTitle === null || newTitle.trim() === "") return;
+  let formattedComletedDate;
+  const formattedCreatedDate = formatTodoDate(todo.createdAt);
+  if (todo.completedAt && todo.completed) {
+    formattedComletedDate = formatTodoDate(todo.completedAt);
+  }
 
-    const newPriority = prompt(
-      "Введіть пріоритет (high, medium, low):",
-      todo.priority
-    );
-    if (!["high", "medium", "low"].includes(newPriority)) return;
+  const handleOpenEditModal = () => {
+    setIsEditModalOpen(true);
+  };
 
-    const newColor = prompt(
-      "Введіть колір (hex або назва кольору):",
-      todo.color
-    );
-    if (newColor === null || newColor.trim() === "") return;
+  const handleCloseEditModal = () => {
+    setIsEditModalOpen(false);
+  };
 
-    dispatch(
-      updateTodo({
-        id: todo.id,
-        title: newTitle,
-        priority: newPriority,
-        color: newColor,
-      })
-    );
+  const handleCloseDeleteModal = () => {
+    setIsDeleteModalOpen(false);
+  };
+
+  const handleOpenDeleteModal = () => {
+    setIsDeleteModalOpen(true);
+  };
+
+  const handleSaveEdit = ({ title, priority, color, projectId }) => {
+    const updatedItem = {
+      id: todo.id,
+      title,
+      priority,
+      color,
+      projectId,
+    };
+    dispatch(updateTodo(updatedItem));
+    handleCloseEditModal();
   };
 
   return (
@@ -59,7 +63,6 @@ const TodoItem = ({ todo, project }) => {
             : "border-gray-300 dark:border-gray-600"
         } transition`}
       />
-
       <div className="flex-1 min-w-0">
         <div className="flex justify-between items-start gap-2">
           <h3
@@ -76,7 +79,7 @@ const TodoItem = ({ todo, project }) => {
               className="edit-todo text-gray-400 hover:text-blue-500 transition"
               data-todo-id={todo.id}
               aria-label="Редагувати завдання"
-              onClick={handleEdit}
+              onClick={handleOpenEditModal}
             >
               <FaEdit />
             </button>
@@ -84,14 +87,28 @@ const TodoItem = ({ todo, project }) => {
               className="text-gray-400 hover:text-red-500 transition"
               data-todo-id={todo.id}
               aria-label="Видалити завдання"
-              onClick={() => dispatch(deleteTodo(todo.id))}
+              onClick={handleOpenDeleteModal}
             >
               <FaTrashAlt />
             </button>
           </div>
         </div>
 
-        <div className="mt-2 flex flex-wrap items-center gap-2">
+        <div className="mt-2 flex flex-col items-start justify-start  gap-2">
+          <div className=" flex flex-wrap items-center gap-2">
+            <span
+              className={`px-2 py-1 w-[46px] text-center text-xs rounded-full p ${getPriorityClass(
+                todo.priority
+              )}`}
+            >
+              {todo.priority}
+            </span>
+
+            <span className="text-xs text-gray-500 dark:text-gray-400">
+              {formattedCreatedDate}{" "}
+              {todo.completedAt && <span>- {formattedComletedDate}</span>}
+            </span>
+          </div>
           {project && (
             <span
               className="px-2 py-1 text-xs rounded-full"
@@ -100,19 +117,36 @@ const TodoItem = ({ todo, project }) => {
               {project.name}
             </span>
           )}
-
-          <span
-            className={`px-2 py-1 text-xs rounded-full ${getPriorityClass(
-              todo.priority
-            )}`}
-          >
-            {todo.priority}
-          </span>
-          <span className="text-xs text-gray-500 dark:text-gray-400">
-            {todo.createdAt}
-          </span>
         </div>
       </div>
+      <TodoEditModal
+        isOpen={isEditModalOpen}
+        onClose={handleCloseEditModal}
+        todo={todo}
+        currentProject={project}
+        onSave={handleSaveEdit}
+      />
+      <Modal isOpen={isDeleteModalOpen} onClose={handleCloseDeleteModal}>
+        <div className="flex flex-col justify-center   max-w-[350px] text-white p-5 gap-3">
+          <h3 className="text-2xl">Задання буде видалено!</h3>
+          <div className="flex justify-center gap-6 mt-4">
+            <button
+              type="button"
+              onClick={handleCloseDeleteModal}
+              className="px-4 py-2 bg-gray-200 text-gray-800 rounded-md hover:bg-gray-300 transition"
+            >
+              Скасувати
+            </button>
+            <button
+              type="button"
+              onClick={() => dispatch(deleteTodo(todo.id))}
+              className="px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700 transition"
+            >
+              Видалити
+            </button>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 };
