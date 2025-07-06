f5bcac07ca9b2171d7b85fc24594d098d2021bed
sokilskills <vprovorotov@amil.com>
2025-07-06 22:54:31 +0300

    feat(refact): update button
    1) додано іконку - кнопки закриття модального вікна
    2) використано компонент Button
    3) Оновлено падінги модального вікна

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import ReactDOM from "react-dom";
+import { IoClose } from "react-icons/io5";
+import Button from "./Button";
 
 const modalRoot = document.getElementById("modal-root");
 
@@ -24,19 +26,21 @@ const Modal = ({ children, isOpen, onClose }) => {
 
   return ReactDOM.createPortal(
     <div
-      className="fixed left-0 bottom-0 top-0 right-0 bg-gray-800/30 px-5 flex justify-center items-center z-[1000]"
+      className="fixed left-0 bottom-0 top-0 right-0 bg-gray-800/30 px-5 sm:p-8 md:px-11 flex justify-center items-center z-[1000]"
       onClick={onClose}
     >
       <div
-        className="relative dark:bg-gray-900 p-5 rounded-lg shadow-[0 4px 6px rgba(0, 0, 0, 0.1)] max-w-4xl "
+        className="relative dark:bg-gray-900 p-5 sm:p-8 md:p-10 md:py-12 rounded-lg shadow-[0 4px 6px rgba(0, 0, 0, 0.1)] max-w-4xl "
         onClick={(e) => e.stopPropagation()}
       >
-        <button
-          className="absolute top-3.5 right-3.5 flex justify-center items-center bg-none text-2xl text-amber-700 w-5 h-5"
+        <Button
+          className="absolute top-3.5 right-3.5 flex justify-center items-center  bg-none text-amber-700 w-8 h-8 md:top-7 md:right-7 "
+          size=""
+          variant=""
           onClick={onClose}
         >
-          &times;
-        </button>
+          <IoClose size={"20px"} />
+        </Button>
         {children}
       </div>
     </div>,
