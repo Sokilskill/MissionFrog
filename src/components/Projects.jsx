05264ac172ef3069c6a4c958af1d07470536de91
sokilskills <vprovorotov@amil.com>
2025-07-06 22:56:55 +0300

    feat(refact): update button
    1) Використано компонент Button

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,6 +4,7 @@ import ProjectModal from "./ProjectModal";
 import { useDispatch, useSelector } from "react-redux";
 import { selectProjects } from "../redux/projects/projectSelector";
 import { addSelectedProjectId } from "../redux/filters/filtersSlice";
+import Button from "./Button";
 
 const Projects = () => {
   const [showModal, setShowModal] = useState(false);
@@ -29,13 +30,16 @@ const Projects = () => {
       <div className="flex justify-between items-center mb-4">
         <h2 className="font-semibold text-lg">Проекти</h2>
 
-        <button
-          className="text-blue-500 hover:text-blue-700 transition"
+        <Button
+          variant=""
+          className="text-blue-500 hover:text-blue-700"
+          size="sm"
+          aria-label="Додати новий проєкт"
           onClick={() => setShowModal(true)}
-        >
-          <FaPlus />
-        </button>
+          icon={FaPlus}
+        ></Button>
       </div>
+
       <ul
         className="space-y-2 overflow-auto max-h-[200px] scrollbar scrollbar-thumb-gray-400 scrollbar-track-gray-200
                    dark:scrollbar-thumb-gray-600 dark:scrollbar-track-gray-700
@@ -56,16 +60,14 @@ const Projects = () => {
             <div className="flex justify-between">
               {project.name}
 
-              <div>
-                <button
-                  aria-label="Редагувати"
-                  data-project-id={project.id}
-                  className="hover:text-blue-500"
-                  onClick={() => {}}
-                >
-                  <FaEdit />
-                </button>
-              </div>
+              <Button
+                variant="icon"
+                size="icon"
+                data-todo-id={project.id}
+                aria-label="Редагувати проєкт"
+                onClick={() => {}}
+                icon={FaEdit}
+              ></Button>
             </div>
           </li>
         ))}
