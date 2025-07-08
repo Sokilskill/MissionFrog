84149403985b9fe28bbd03dc18c82896ef30e850
sokilskills <vprovorotov@amil.com>
2025-07-08 19:32:12 +0300

    feat: add hook useTheme
    1) Логіка зміни теми темна/світла

---
+++ src/hooks/useTheme.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+export const useTheme = () => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    const stored = localStorage.getItem("theme");
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    console.log(stored);
+
+    const initialTheme = stored || (prefersDark ? "dark" : "light");
+    setTheme(initialTheme);
+    document.documentElement.classList.toggle("dark", initialTheme === "dark");
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "dark" ? "light" : "dark";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+    document.documentElement.classList.remove("dark");
+    if (newTheme === "dark") {
+      document.documentElement.classList.add("dark");
+    }
+  };
+
+  return { theme, toggleTheme };
+};
