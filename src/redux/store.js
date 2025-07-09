3457fa177c1f0892e792f0b51dc147e268fb3ce9
sokilskills <vprovorotov@amil.com>
2025-07-09 22:44:08 +0300

    feat: add Persist store
    1) Зберігання та отримання даних в local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,43 @@
-import { configureStore } from "@reduxjs/toolkit";
-import projectsReduser from "./projects/projectsSlice";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import projectsReducer from "./projects/projectsSlice";
 import todosReducer from "./todos/todosSlice";
-import filterReduce from "./filters/filtersSlice";
+import filtersReducer from "./filters/filtersSlice";
+
+const rootReducer = combineReducers({
+  projects: projectsReducer,
+  todos: todosReducer,
+  filters: filtersReducer,
+});
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["projects", "todos"],
+  blacklist: ["filters"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    projects: projectsReduser,
-    todos: todosReducer,
-    filters: filterReduce,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
+export const persistor = persistStore(store);
 export default store;
